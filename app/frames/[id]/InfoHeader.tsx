b3f606326033e94fbe3cec199c289e3b2c894a90
Michael J. Cohen <michaelcohen716@gmail.com>
2024-07-26 12:23:48 -0700

    continue

--- app/frames/[id]/InfoHeader.tsx
+++ app/frames/[id]/InfoHeader.tsx
@@ -18,25 +18,25 @@ function InfoHeader({ users, booking }: any) {
   const [owner, booker] = users;
   return (
     <div
-      tw="flex px-4 py-8 justify-between"
+      tw="flex px-4 py-4 justify-between"
       style={{
         marginTop: "-60px",
       }}
     >
       <div tw="flex">
-        <div tw="flex h-[100px] w-[100px]">
+        <div tw="flex h-[60px] w-[60px]">
           <img
             src={owner.pfp_url}
             alt="Profile"
             tw="flex h-full w-full object-cover rounded-full"
           />
         </div>
-        <div tw="flex flex-col ml-3">
+        <div tw="flex flex-col ml-3 mt-[-8px]">
           <div tw="flex">{owner.display_name}</div>
-          <div tw="flex text-[28px] text-[#9A9898]">@{owner.username}</div>
+          <div tw="flex text-[24px] text-[#9A9898]">@{owner.username}</div>
         </div>
       </div>
-      <div tw="flex flex-col text-[26px]">
+      <div tw="flex flex-col text-[22px]">
         <div tw="flex">
           <div>Booked by&nbsp;</div>
           <div tw="flex mx-1 text-[#1F28FF]">&nbsp;@{booker.username}{'  '}</div>
--- app/frames/[id]/route.tsx
+++ app/frames/[id]/route.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable react/jsx-key */
 import { Button } from "frames.js/next";
 import { frames } from "../frames";
-import { fetchBooking, fetchPreview, fetchBulkUsers, getCurrentSignal } from "../../services";
+import {
+  fetchBooking,
+  fetchPreview,
+  fetchBulkUsers,
+  getCurrentSignal,
+} from "../../services";
 import InfoHeader from "./InfoHeader";
 import { farcasterHubContext } from "frames.js/middleware";
-
 
 function Pfp({ url }: any) {
   return (
@@ -20,7 +24,7 @@ const frameHandler = frames(async (ctx) => {
   if (!activityId) throw Error("Invalid param");
   const { content, booking } = await fetchBooking(activityId);
 
-  const currentSignal = await getCurrentSignal(activityId)
+  const currentSignal = await getCurrentSignal(activityId);
 
   const bookerFid = Number((booking as any).bookerFid);
   const ownerFid = activityId.split("-")[0];
@@ -30,6 +34,9 @@ const frameHandler = frames(async (ctx) => {
   ]);
   // @ts-ignore
   const [owner, booker] = users;
+  console.log("ogData.data.twitterImage", ogData.data.twitterImage);
+  console.log("ogData.data.ogImage", ogData.data.ogImage);
+  console.log("ogData.preprocessedImage", ogData.preprocessedImage);
 
   const description = ogData.data.ogDescription;
   const siteName = ogData.data.ogSiteName || ogData.data.alIphoneAppName;
@@ -45,27 +52,32 @@ const frameHandler = frames(async (ctx) => {
       >
         <InfoHeader users={users} booking={booking} />
         {ogData.preprocessedImage && (
-          <img src={ogData.preprocessedImage} tw="w-full" />
-        )}}
+          <img src={ogData.preprocessedImage} tw=" w-full" />
+        )}
         <div
           tw="py-4 px-5 flex flex-col"
           style={{
             backgroundColor: "#181A1C",
           }}
         >
-          <div tw="text-[28px] text-[#9A9898]">{siteName}</div>
-          <div tw="font-bold text-[40px] my-3 text-white">{title}</div>
-          <div tw="text-[28px] text-[#CECDCD]">{description.slice(0, 75) + "..."}</div>
+          <div tw="text-[24px] text-[#9A9898]">{siteName}</div>
+          <div tw="font-bold text-[34px] my-3 text-white">{title}</div>
+          <div tw="text-[24px] text-[#CECDCD]">{description.slice(0, 75) + "..."}</div>
         </div>
-        <div tw="flex justify-center my-6 font-bold">Signal: {currentSignal}</div>
-        <div tw="flex justify-center mt-8">
-          <Pfp url={owner.pfp_url} />
-          <Pfp url={booker.pfp_url} />
-          <Pfp url={owner.pfp_url} />
-          <Pfp url={booker.pfp_url} />
-          <Pfp url={owner.pfp_url} />
-          <Pfp url={booker.pfp_url} />
-          <div>and 8 others</div>
+        <div tw="flex justify-around text-[36px] items-center bg-[#181A1C] text-white pt-4 pb-6">
+          <div tw="flex font-extrabold">
+            Signal: {currentSignal}
+          </div>
+
+          <div tw="flex justify-center">
+            <Pfp url={owner.pfp_url} />
+            <Pfp url={booker.pfp_url} />
+            <Pfp url={owner.pfp_url} />
+            <Pfp url={booker.pfp_url} />
+            <Pfp url={owner.pfp_url} />
+            <Pfp url={booker.pfp_url} />
+            <div tw="ml-2">and 8 others</div>
+          </div>
         </div>
       </div>
     ),
